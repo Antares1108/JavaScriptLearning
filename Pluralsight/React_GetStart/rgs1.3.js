6924c2c56f9f08961de2de37226a418b847d48c0
Antares1108 <35879267+Antares1108@users.noreply.github.com>
2019-03-07 13:37:00 -0500

    Create rgs1.3.js

---
+++ Pluralsight/React_GetStart/rgs1.3.js
@@ -0,0 +1,36 @@
+// Challenge: Start the button with 5
+// Double the button's label on each click
+
+function Button(props) {
+	return (
+  	<button onClick={props.onClickFunction}>
+    	+1
+    </button>
+  );
+}
+
+function Display(props) {
+	return (
+  	<div>
+    	{props.message}
+    </div>
+  );
+
+}
+
+// App
+function App() {
+	const [counter, setCounter] = useState(42);
+  const incrementCounter = () => setCounter(counter + 1);
+  return (
+  	<div>
+    	<Button onClickFunction={incrementCounter}/>
+      <Display message={counter} />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('mountNode'),
+);
