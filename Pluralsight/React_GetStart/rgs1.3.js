5f63cc5617b6c2802108afb183e22324d64f7472
Antares1108 <35879267+Antares1108@users.noreply.github.com>
2019-03-07 13:39:09 -0500

    Update rgs1.3.js

--- Pluralsight/React_GetStart/rgs1.3.js
+++ Pluralsight/React_GetStart/rgs1.3.js
@@ -2,35 +2,35 @@
 // Double the button's label on each click
 
 function Button(props) {
-	return (
-  	<button onClick={props.onClickFunction}>
-    	+1
-    </button>
-  );
+    return (
+        <button onClick={props.onClickFunction}>
+            +1
+        </button>
+    );
 }
 
 function Display(props) {
-	return (
+    return (
   	<div>
-    	{props.message}
-    </div>
-  );
+    	    {props.message}
+        </div>
+    );
 
 }
 
 // App
 function App() {
-	const [counter, setCounter] = useState(42);
-  const incrementCounter = () => setCounter(counter + 1);
-  return (
+    const [counter, setCounter] = useState(42);
+    const incrementCounter = () => setCounter(counter + 1);
+    return (
   	<div>
-    	<Button onClickFunction={incrementCounter}/>
-      <Display message={counter} />
-    </div>
-  );
+    	    <Button onClickFunction={incrementCounter}/>
+            <Display message={counter} />
+        </div>
+    );
 }
 
 ReactDOM.render(
-  <App />, 
-  document.getElementById('mountNode'),
+    <App />, 
+    document.getElementById('mountNode'),
 );
